5788aed8dceea46502cc6004c99efe551649bc47
Aniket Bhattacherjee <aniketbhattacherjee1990@gmail.com>
2020-02-22 16:32:31 +0530

    Modified route as customer

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/customer', (req, res) => res.send({
+    customerName: 'Arindam'
+}))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
