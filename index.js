045d8a1762d27fb4202322d8236b23db62e02150
Aniket Bhattacherjee <aniketbhattacherjee1990@gmail.com>
2020-02-23 09:37:17 +0530

    Changed port number to 8081

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 8081
 
 app.get('/customer', (req, res) => res.send({
     customerName: 'Arindam'
